758bb58b4d4d2cb096725da69f68efb1420a8e26
Tanya Parmar <tanya.parmar@thoughtworks.com>
2021-09-18 16:20:16 +0200

    Add weather information for the cities

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -4,14 +4,26 @@ import { Card } from 'semantic-ui-react'
 export default function WeatherCard({weatherData}) {
     return (
         <Card.Group>
-            <Card>
-                <Card.Content>
-                    <Card.Header className="header">{weatherData.name}</Card.Header>
-                    <Card.Description>
-                        This is a weather card
-                    </Card.Description>
-                </Card.Content>
-            </Card>
+            {weatherData.map((weatherInfo) => (
+                <Card key={weatherInfo.id}>
+                    <Card.Content>
+                        <Card.Header className="header">{weatherInfo.name}</Card.Header>
+                        <Card.Description>
+                            Temperature: {weatherInfo.main.temp} <br/>
+                            Max Temperature: {weatherInfo.main.temp_max} <br/>
+                            Min Temperature: {weatherInfo.main.temp_min} <br/>
+                            Sunrise: {weatherInfo.sys.sunrise} <br/>
+                            Sunset: {weatherInfo.sys.sunset} <br/>
+                            Visibility: {weatherInfo.visibility} <br/>
+                            Weather: {weatherInfo.weather[0].description} <br/>
+                        </Card.Description>
+                        <div className="ui bottom attached button">
+                            <button className="ui button" onClick={(console.log('clicked'))}>Weather Detail</button>
+                        </div>
+                    </Card.Content>
+                </Card>
+            ))}
+
         </Card.Group>
     )
 }
--- src/services/WeatherInfoService.js
+++ src/services/WeatherInfoService.js
@@ -1,31 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import WeatherCard from "../components/WeatherCard";
+import axios from "axios";
 
 export const baseUrl = 'https://api.openweathermap.org/data/2.5';
 export const apiKey = process.env.REACT_APP_API_KEY;
 
 export default function WeatherInfoService() {
+    const [latitude, setLatitude] = useState([]);
+    const [longitude, setLongitude] = useState([]);
     const [data, setData] = useState([]);
 
+    // navigator.geolocation.getCurrentPosition(function(showPosition) {
+    //     setLatitude(showPosition.coords.latitude);
+    //     setLongitude(showPosition.coords.longitude);
+    // });
+
+    const London =  `${baseUrl}/weather?q=London,uk&APPID=${apiKey}`;
+    const Berlin = `${baseUrl}/weather?q=Berlin, de&APPID=${apiKey}`;
+    // const Current = `${baseUrl}/weather/?lat=${latitude}&lon=${longitude}&units=metric&APPID=${apiKey}`;
+
     useEffect(() => {
-        const getCityWeatherInfo = async (city) => {
-            await fetch(`${baseUrl}/weather?q=${city}&APPID=${apiKey}`)
-                .then(res => res.json())
-                .then(result => {
-                    setData(result)
-                    console.log(result)
-                });
-        }
-        getCityWeatherInfo("Berlin")
+        const getWeatherInformation = async () => {
+
+            const requestLondon = axios.get(London);
+            const requestBerlin = axios.get(Berlin);
+            // const requestCurrent = axios.get(Current)
+            try{
+                await axios.all([ requestLondon, requestBerlin])
+                    .then(axios.spread((...responses) => {
+                        const firstResponse = responses[0]
+                        const secondResponse = responses[1]
+                        // const thirdResponse = responses[2]
+                        // console.log(firstResponse.data,secondResponse.data);
+                        setData([firstResponse.data, secondResponse.data])
+                        console.log([firstResponse, secondResponse])
+                    }))
+            } catch(error)  {
+                throw error;
+            }
+
+        };
+        getWeatherInformation()
     }, []);
 
     return (
         <div className="App">
-            {(typeof data.main != 'undefined') ? (
+            <div>
                 <WeatherCard weatherData={data}/>
-            ): (
-                <div></div>
-            )}
+            </div>
         </div>
     );
 }
